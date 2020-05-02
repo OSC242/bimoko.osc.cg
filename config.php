4e88798f829573a68a6a02d2bd1a5c0b5cdc1e5a
Adnan RIHAN <adnan@rihan.fr>
2020-05-02 13:11:42 +0200

    configs

--- config.php
+++ config.php
@@ -5,14 +5,14 @@ use Illuminate\Support\Str;
 return [
     'baseUrl' => '',
     'production' => false,
-    'siteName' => 'Blog Starter Template',
-    'siteDescription' => 'Generate an elegant blog with Jigsaw',
-    'siteAuthor' => 'Author Name',
+    'siteName' => 'Bimiko Podcast',
+    'siteDescription' => "Le podcast congolais où on démystifie l'actualité tech",
+    'siteAuthor' => 'Adnan RIHAN <adnan@osc.cg>',
 
     // collections
     'collections' => [
         'posts' => [
-            'author' => 'Author Name', // Default author, if not provided in a post
+            'author' => 'Adnan R.', // Default author, if not provided in a post
             'sort' => '-date',
             'path' => 'blog/{filename}',
         ],
