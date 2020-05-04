3eb3a4c43a99faf4662625d2576d3d1765214029
Adnan RIHAN <adnan@rihan.fr>
2020-05-04 02:43:09 +0200

    Removed about page

--- source/_nav/menu.blade.php
+++ source/_nav/menu.blade.php
@@ -4,9 +4,6 @@
         Blog
     </a>
 
-    <a title="{{ $page->siteName }} About" href="/about"
-        class="ml-6 text-gray-700 hover:text-blue-600 {{ $page->isActive('/about') ? 'active text-blue-600' : '' }}">
-        About
     </a>
 
     <a title="{{ $page->siteName }} Contact" href="/contact"
